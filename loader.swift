b7e36a25b46747754937ea3278444904e8ab107a
collectionman <julianllorensi@outlook.com>
2019-09-14 16:28:02 -0300

    Example of loader class

---
+++ loader.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+protocol FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void)
+}
+
+class RemoteFeedLoader : FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // remote feedback
+    }
+}
+
+class LocalFeedLoader : FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // local feedback
+    }
+}
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+}
+
+struct Reacheability {
+    static let networkAvailable = false 
+
+    func getStatus() {
+        networkAvailable = checkForNetworkStatus()
+    }
+
+    private func checkForNetworkStatus() -> Bool {
+        return NetworkManager.shared.state == .available ? true : false
+    }
+}
+
+class RemoteWithLocalFallbackFeedLoader : FeedLoader {
+    let remote: RemoteFeedLoader
+    let local: LocalFeedLoader
+
+    init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.remote = remote
+        self.local = local
+    }
+
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        let load = Reacheability.networkAvailable? remote.loadFeed : local.loadFeed
+        load(completion) 
+    }
+}
+
+class FeedViewController : UIViewController {
+    var loader: FeedLoader!
+
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader.loadFeed { loadedItems in
+            // Update UI
+        }
+    }
+}
